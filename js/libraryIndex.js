965d9c67f76c8df012699665b0a16df4128a5131
00Land00 <lander.vitug@gmail.com>
2024-07-07 22:39:14 -0500

    implemented the progress bar and checkbox logic

--- js/libraryIndex.js
+++ js/libraryIndex.js
@@ -7,17 +7,18 @@ const libraryIndex = (function () {
 
   const sidebarBooks = document.querySelector(".books");
 
-  const bookTitle = document.querySelector(".book-title");
+  const bookTitle = document.querySelector(".book-header .book-title");
   const bookAuthor = document.querySelector(".book-author");
   const bookDesc = document.querySelector(".book-desc");
-  const progressText = document.querySelector(".progress-text");
+  const progressValue = document.querySelector(".progress-value");
   const progressBar = document.querySelector(".progress-bar");
   const completeCheckbox = document.querySelector(".complete-checkbox");
 
   const addBtn = document.querySelector(".add-btn");
   const delBtn = document.querySelector(".book-del");
 
-  let curId = -1;
+  let curBook = null;
+  let curSidebarProgress = null;
 
   module.addBook = (bookObj) => {
     // for now we will just append it at the end. will want to sort it in alphabetical order though
@@ -37,19 +38,36 @@ const libraryIndex = (function () {
     return books.find((book) => book.dataId === id);
   };
 
+  const updateProgressVal = () => {
+    progressValue.innerHTML = `${progressBar.value} / ${curBook.totalPages}`;
+    curBook.curPage = progressBar.value;
+    const progressPercent = curBook.calcProgress();
+    curSidebarProgress.innerHTML = `${progressPercent}%`;
+
+    progressValue.style.left = `calc(${progressPercent}% + (${17 - progressPercent * 0.3}px))`;
+
+    completeCheckbox.checked = (curBook.curPage === curBook.totalPages);
+  };
+
+  const updateProgressBar = () => {
+    progressBar.max = curBook.totalPages;
+    progressBar.value = curBook.curPage;
+  };
+
   const populateBookInfo = (book) => {
     bookTitle.innerHTML = `${book.title}`;
     bookAuthor.innerHTML = `${book.author}`;
     bookDesc.innerHTML = `${book.desc}`;
-    progressText.innerHTML = `${book.curPage} / ${book.totalPages}`;
-    // progressBar
+    updateProgressBar();
+    updateProgressVal();
     completeCheckbox.checked = book.hasCompleted;
   };
 
   const sidebarClickEH = (e) => {
     const bookId = parseInt(e.currentTarget.dataset.id);
     const book = findBook(bookId);
-    curId = bookId;
+    curBook = book;
+    curSidebarProgress = curBook.sidebarHTML.querySelector(".progress");
 
     populateBookInfo(book);
     index.toggleForm(false);
@@ -57,23 +75,36 @@ const libraryIndex = (function () {
 
   const deleteEH = () => {
     // TODO: add modal
-    const booksId = books.findIndex((book) => book.dataId === curId);
-    const book = books.splice(booksId, 1)[0];
-    book.sidebarHTML.remove();
-    book.sidebarHTML = null;
+    books.splice(curBook.dataId, 1)[0];
+    curBook.sidebarHTML.remove();
+    curBook.sidebarHTML = null;
+    curBook = null;
+    curSidebarProgress = null;
 
     index.toggleForm(true);
   };
 
   const formBtnEH = () => {
-    curId = null;
+    curBook = null;
+    curSidebarProgress = null;
     index.toggleForm(true);
+  };
+
+  const checkboxEH = () => {
+    curBook.hasCompleted = completeCheckbox.checked ? true : false;
+    if (curBook.hasCompleted) {
+      curBook.curPage = curBook.totalPages;
+      progressBar.value = curBook.curPage;
+      updateProgressVal();
+    }
   };
 
   window.addEventListener("DOMContentLoaded", () => {
     displaySidebar();
     addBtn.addEventListener("click", formBtnEH);
     delBtn.addEventListener("click", deleteEH);
+    progressBar.addEventListener("input", updateProgressVal);
+    completeCheckbox.addEventListener("change", checkboxEH);
   });
 
   return module;
